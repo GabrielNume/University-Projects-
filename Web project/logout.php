71063f9168a54685e776629ac075767e89a78d40
GabrielNume <75905074+GabrielNume@users.noreply.github.com>
2022-11-03 20:49:29 +0200

    Delete Web project directory

--- Web project/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-if(isset($_POST['submit'])){
-    require_once './login_delegator.php';
-    $loginDelegator = new LoginDelegator(new Validator);
-    $loginDelegator->logout();
-    header('location: ./index.php');
-    exit();
-}
